5a66d60b73f4f7c7791e18663f164249e9c7341b
glieseKep <starneko89@gmail.com>
2026-01-15 02:08:56 -0500

    Arreglos para que funcione postman

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,18 +1,15 @@
 import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
 
 export class RegisterDto {
+  @IsString()
+  @MinLength(3)
+  nombre: string;
+
   @IsEmail({}, { message: 'El correo electrónico no es válido' })
   email: string;
 
   @IsString()
-  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
-  @MaxLength(20, {
-    message: 'El tamaño máximo de la contraseña es 20 caracteres',
-  })
+  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres.' })
+  @MaxLength(20)
   contrasenia: string;
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  nombre: string;
 }
