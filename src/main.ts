b400afe3755255f93a265ab1c250ec9b1e28c942
glieseKep <starneko89@gmail.com>
2026-01-15 00:21:35 -0500

    Validación global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilitar validación automática de DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades no definidas en el DTO
+      transform: true, // Transforma tipos automáticamente
+    }),
+  );
+
+  await app.listen(3000);
+  console.log('Servidor corriendo en http://localhost:3000');
 }
 bootstrap();
